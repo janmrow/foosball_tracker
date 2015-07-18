746c3cb100e0edf4cc54dbc571aa94c4d294a29c
Jasiek <jasiek92@gmail.com>
2015-07-18 16:58:22 +0200

    add players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,21 @@
 class PlayersController < ApplicationController
+  
   def index
+    @players = Player.all
   end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.create(player_params)
+    redirect_to :root
+  end
+
+  private
+
+    def player_params
+      params.require(:player).permit(:firstname, :lastname)
+    end
 end
