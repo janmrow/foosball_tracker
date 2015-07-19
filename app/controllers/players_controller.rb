ee75e2d4a370b3e37e8a76e9dec497d61c477462
Jasiek <jasiek92@gmail.com>
2015-07-19 12:02:47 +0200

    show page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,6 +4,10 @@ class PlayersController < ApplicationController
     @players = Player.all
   end
 
+  def show
+    @player = Player.find(params[:id])
+  end
+
   def new
     @player = Player.new
   end
