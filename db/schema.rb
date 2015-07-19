42b0362571e8aefcfbee2ed05fb870970973a370
Jasiek <jasiek92@gmail.com>
2015-07-19 16:12:22 +0200

    add nickname filed to Player

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719133454) do
+ActiveRecord::Schema.define(version: 20150719141111) do
 
   create_table "matches", force: true do |t|
     t.integer  "winner_player_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150719133454) do
     t.string   "avatar"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
   end
 
 end
