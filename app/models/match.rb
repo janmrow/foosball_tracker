c81377568e2c47acbb912bc0c4427a9bc12918da
Jasiek <jasiek92@gmail.com>
2015-07-26 15:31:28 +0200

    add custom validation to match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,9 @@
 class Match < ActiveRecord::Base
+  validate :no_match_with_himself
+
+  def no_match_with_himself
+    if winner_player_id == loser_player_id
+      errors.add(:match, "can't be between the same player")
+    end
+  end
 end
