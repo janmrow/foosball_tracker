49ab7deeb64112e12dcc47d06484133a3e3e5fb9
Jasiek <jasiek92@gmail.com>
2015-07-27 11:25:37 +0200

    improve tests for adding players

--- spec/requests/add_players_spec.rb
+++ spec/requests/add_players_spec.rb
@@ -1,29 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "AddPlayers", type: :request do
-  describe "add player" do
-    let(:firstname) { "John" }
-    let(:lastname) { "Smith" }
-    let(:nickname) { "smithy99" }
+  describe "Creating player" do
 
-    it "add new player with valid data" do
-      visit new_player_path
-      fill_in "player_firstname", with: firstname
-      fill_in "player_lastname", with: lastname
-      fill_in "player_nickname", with: nickname
+    subject { page }
 
-      click_button "Add player"
-      Player.all.count.should eq 1
-      @player = Player.first
-      @player.nickname.should eq nickname
-      current_path.should eq player_path(1)
-      page.should have_content(nickname)
+    describe "players page" do
+      before { visit new_player_path }
+
+      it { should have_selector('h1', text: 'Add new player') }
     end
 
-    it "add players without nickname" do
-      visit new_player_path
-      click_button "Add player"
-      current_path should eq new_player_path
+    describe "create new player" do
+      before { visit new_player_path }
+
+
+      describe "with valid data" do
+        let(:firstname) { "John" }
+        let(:lastname) { "Smith" }
+        let(:nickname) { "smithy99" }
+
+        before do 
+          fill_in "player_firstname", with: firstname
+          fill_in "player_lastname", with: lastname
+          fill_in "player_nickname", with: nickname
+          click_button "Add player"
+        end
+
+        it { should have_selector('h2', text: nickname ) }
+        it { should have_selector('div.alert', text: "Player created")}
+        # Player.all.count.should eq 1
+        # @player = Player.first
+        # @player.nickname.should eq nickname
+        # current_path.should eq player_path(1)
+        # page.should have_content(nickname)
+      end
+
+      describe "without nickname" do
+        before do
+          visit new_player_path        
+          click_button "Add player"
+        end
+        # current_path should eq new_player_path
+      end
+
+
     end
   end
 end
