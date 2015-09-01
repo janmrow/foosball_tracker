bcc5943ddefde69a830f41a80f84b3cc60e5565e
Jasiek <jasiek92@gmail.com>
2015-09-01 12:49:17 +0200

    redirect to root when no route match

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/about'
   get 'matches/new'
+
   root 'pages#home'
+
   resources :players
   resources :matches
 
-  # maybe some upgrades in routes?
+  get '*path' => redirect('/')
 end
